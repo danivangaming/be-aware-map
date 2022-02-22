01489a310194e0395c0934ad9b6561babf6df668
danivangaming <62762284+danivangaming@users.noreply.github.com>
2022-02-22 08:11:09 +0000

    Deploying to gh-pages from master @ 16616c58df2558d059df28d59d3fb8b5c4d7a929 🚀

--- Scripts/be-aware-api/Api/iframe/state.d.ts
+++ Scripts/be-aware-api/Api/iframe/state.d.ts
@@ -7,7 +7,7 @@ interface DataDescriptor {
 export declare const initVariables: (_variables: Map<string, unknown>) => void;
 export declare class WorkadventureStateCommands extends IframeApiContribution<WorkadventureStateCommands> {
     callbacks: import("./IframeApiContribution").IframeCallbackContribution<keyof import("../Events/IframeEvent").IframeResponseEventMap>[];
-    getLocalStorageData(type: string, data: string, language?: DataDescriptor): void;
+    getLocalStorageData(type: string, data: string, language?: DataDescriptor, level?: DataDescriptor): void;
     saveVariable(key: string, value: unknown): Promise<void>;
     loadVariable(key: string): unknown;
     onVariableChange(key: string): Observable<unknown>;
